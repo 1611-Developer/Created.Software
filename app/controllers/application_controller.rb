1e8b4ebd2108ca039a30c438f94b130204696864
Alexander Morrison <1611.developer@gmail.com>
2025-09-08 19:08:53 -0700

    style changes to logo and background; Main text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  
-  # A helper method that returns the base title.  
+
+  # A helper method that returns the base title.
   helper_method :base_title
   def base_title
     "Created.Software"
