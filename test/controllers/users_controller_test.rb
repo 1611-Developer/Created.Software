6b67fe7087c4f18cf29c277a0069dd5bdc9f0efc
Alexander Morrison <1611.developer@gmail.com>
2025-09-09 12:11:10 -0700

    Updated user routes in routes.rb, /user_controller_test.rb; updated nav link in homepage w/ new signup route; updated signup page from default; updated user_controller_test.rb with base_title; changed logo font

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Created.Software"
+  end
+
   test "should get new" do
-    get users_new_url
+    get signup_path
+    assert_select "title", "Signup | #{@base_title}"
     assert_response :success
   end
 end
