146febd3d2f6932de414dae49ae07d22a5bd7c88
Alexander Morrison <1611.developer@gmail.com>
2025-09-11 12:26:04 -0700

    Validating Email Uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_10_023759) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_11_190502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,5 +22,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_10_023759) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
